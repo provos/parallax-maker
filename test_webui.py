be3939fdb8c85cae3285fea615b2adc9e2cbdea4
provos <provos@gmail.com>
2024-04-12 10:22:02 -0700

    move to regular python unittest framework

--- test_webui.py
+++ test_webui.py
@@ -23,45 +23,42 @@ class TestFilenameAddVersion(unittest.TestCase):
         expected = "dir/image_v2.png"
         self.assertEqual(filename_add_version(filename), expected)
 
-def test_update_threshold_values_boundaries():
-    # Create a dummy state
-    state = AppState()
-    state.imgThresholds = [0, 10, 20, 30, 40, 255]
+class TestUpdateThresholds(unittest.TestCase):
+    def test_update_threshold_values_boundaries(self):
+        # Create a dummy state
+        state = AppState()
+        state.imgThresholds = [0, 10, 20, 30, 40, 255]
 
-    # set up a fake cache
-    filename = 'teststate'
-    state.cache[filename] = state
+        # set up a fake cache
+        filename = 'teststate'
+        state.cache[filename] = state
 
-    # Call the function
-    input_thresholds = [0, 20, 30, 40, 255]
-    num_slices = 5
-    threshold_values, _ = update_threshold_values(input_thresholds, num_slices, filename)
+        # Call the function
+        input_thresholds = [0, 20, 30, 40, 255]
+        num_slices = 5
+        threshold_values, _ = update_threshold_values(input_thresholds, num_slices, filename)
 
-    # Assert that the state is updated
-    print(threshold_values)
-    print(state.imgThresholds)
-    assert state.imgThresholds == [0, 1, 20, 30, 40, 254, 255]
+        # Assert that the state is updated
+        self.assertEqual(state.imgThresholds, [0, 1, 20, 30, 40, 254, 255])
 
 
-def test_update_threshold_values_limit():
-    # Create a dummy state
-    state = AppState()
-    state.imgThresholds = [0, 10, 20, 30, 40, 255]
+    def test_update_threshold_values_limit(self):
+        # Create a dummy state
+        state = AppState()
+        state.imgThresholds = [0, 10, 20, 30, 40, 255]
 
-    # set up a fake cache
-    filename = 'teststate'
-    state.cache[filename] = state
+        # set up a fake cache
+        filename = 'teststate'
+        state.cache[filename] = state
 
-    # Call the function
-    input_thresholds = [255, 255, 255, 255, 255]
-    num_slices = 5
-    threshold_values, _ = update_threshold_values(
-        input_thresholds, num_slices, filename)
+        # Call the function
+        input_thresholds = [255, 255, 255, 255, 255]
+        num_slices = 5
+        threshold_values, _ = update_threshold_values(
+            input_thresholds, num_slices, filename)
 
-    # Assert that the state is updated
-    print(threshold_values)
-    print(state.imgThresholds)
-    assert state.imgThresholds == [0, 255, 256, 257, 258, 254, 255]
+        # Assert that the state is updated
+        self.assertEqual(state.imgThresholds, [0, 255, 256, 257, 258, 254, 255])
 
 
 if __name__ == '__main__':
