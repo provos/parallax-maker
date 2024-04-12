daf115e911503ad6c4c8e3371bef1d2ea9146e24
provos <provos@gmail.com>
2024-04-12 10:05:01 -0700

    Add new test cases for update_threshold_values function

--- test_webui.py
+++ test_webui.py
@@ -1,5 +1,9 @@
 import unittest
-from webui import filename_add_version
+from contextvars import copy_context
+from dash._callback_context import context_value
+from dash._utils import AttributeDict
+from webui import filename_add_version, update_threshold_values
+from controller import AppState
 from pathlib import Path
 
 
@@ -18,6 +22,46 @@ class TestFilenameAddVersion(unittest.TestCase):
         filename = "dir/image.png"
         expected = "dir/image_v2.png"
         self.assertEqual(filename_add_version(filename), expected)
+
+def test_update_threshold_values_boundaries():
+    # Create a dummy state
+    state = AppState()
+    state.imgThresholds = [0, 10, 20, 30, 40, 255]
+
+    # set up a fake cache
+    filename = 'teststate'
+    state.cache[filename] = state
+
+    # Call the function
+    input_thresholds = [0, 20, 30, 40, 255]
+    num_slices = 5
+    threshold_values, _ = update_threshold_values(input_thresholds, num_slices, filename)
+
+    # Assert that the state is updated
+    print(threshold_values)
+    print(state.imgThresholds)
+    assert state.imgThresholds == [0, 1, 20, 30, 40, 254, 255]
+
+
+def test_update_threshold_values_limit():
+    # Create a dummy state
+    state = AppState()
+    state.imgThresholds = [0, 10, 20, 30, 40, 255]
+
+    # set up a fake cache
+    filename = 'teststate'
+    state.cache[filename] = state
+
+    # Call the function
+    input_thresholds = [255, 255, 255, 255, 255]
+    num_slices = 5
+    threshold_values, _ = update_threshold_values(
+        input_thresholds, num_slices, filename)
+
+    # Assert that the state is updated
+    print(threshold_values)
+    print(state.imgThresholds)
+    assert state.imgThresholds == [0, 255, 256, 257, 258, 254, 255]
 
 
 if __name__ == '__main__':
