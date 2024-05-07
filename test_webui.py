48df63fb7a0ff2e2a857d15901e90f5ea10960dd
provos <provos@gmail.com>
2024-05-06 18:20:23 -0700

    fix broken test

--- test_webui.py
+++ test_webui.py
@@ -20,7 +20,7 @@ class TestUpdateThresholds(unittest.TestCase):
         # Call the function
         input_thresholds = [0, 20, 30, 40, 255]
         num_slices = 5
-        threshold_values, _, _ = update_threshold_values(input_thresholds, num_slices, filename)
+        threshold_values, _ = update_threshold_values(input_thresholds, num_slices, filename)
 
         # Assert that the state is updated
         self.assertEqual(state.imgThresholds, [0, 1, 20, 30, 40, 254, 255])
@@ -38,7 +38,7 @@ class TestUpdateThresholds(unittest.TestCase):
         # Call the function
         input_thresholds = [255, 255, 255, 255, 255]
         num_slices = 5
-        threshold_values, _, _ = update_threshold_values(input_thresholds, num_slices, filename)
+        threshold_values, _ = update_threshold_values(input_thresholds, num_slices, filename)
 
         # Assert that the state is updated
         self.assertEqual(state.imgThresholds, [0, 255, 256, 257, 258, 254, 255])
