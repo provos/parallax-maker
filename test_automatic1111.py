9917d4a336cb67f9b80cbfdccc0818df73d64d9f
provos <provos@gmail.com>
2024-05-10 08:47:46 -0700

    write a test case for the models request function

--- test_automatic1111.py
+++ test_automatic1111.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 from PIL import Image
-from automatic1111 import create_img2img_payload
+from automatic1111 import create_img2img_payload, make_models_request
 
 
 class TestCreateImg2ImgPayload(unittest.TestCase):
@@ -47,6 +47,29 @@ class TestCreateImg2ImgPayload(unittest.TestCase):
         self.assertEqual(payload["steps"], self.steps)
         self.assertEqual(payload["cfg_scale"], self.cfg_scale)
         self.assertEqual(payload["mask"], "data:image/png;base64,")
+
+
+class TestMakeModelsRequest(unittest.TestCase):
+    @patch("automatic1111.requests.get")
+    def test_make_models_request(self, mock_get):
+        mock_response = [
+            {'title': 'sd_xl_base_1.0.safetensors [31e35c80fc]', 'model_name': 'sd_xl_base_1.0', 'hash': '31e35c80fc',
+             'sha256': '31e35c80fc4829d14f90153f4c74cd59c90b779f6afe05a74cd6120b893f7e5b', 'filename': 'models/Stable-diffusion/sd_xl_base_1.0.safetensors', 'config': None},
+            {'title': 'sd_xl_refiner_1.0.safetensors [7440042bbd]', 'model_name': 'sd_xl_refiner_1.0', 'hash': '7440042bbd',
+             'sha256': '7440042bbdc8a24813002c09b6b69b64dc90fded4472613437b7f55f9b7d9c5f', 'filename': 'models/Stable-diffusion/sd_xl_refiner_1.0.safetensors', 'config': None},
+            {'title': 'v2-1_768-ema-pruned.ckpt [ad2a33c361]', 'model_name': 'v2-1_768-ema-pruned', 'hash': 'ad2a33c361',
+             'sha256': 'ad2a33c361c1f593c4a1fb32ea81afce2b5bb7d1983c6b94793a26a3b54b08a0', 'filename': 'models/Stable-diffusion/v2-1_768-ema-pruned.ckpt', 'config': None}
+        ]
+
+        mock_get.return_value.json.return_value = mock_response
+
+        server_address = "localhost:7860"
+        models = make_models_request(server_address)
+
+        self.assertEqual(
+            models, ['sd_xl_base_1.0', 'sd_xl_refiner_1.0', 'v2-1_768-ema-pruned'])
+        mock_get.assert_called_once_with(
+            url="http://localhost:7860/sdapi/v1/sd-models", timeout=3)
 
 
 if __name__ == '__main__':
